2fa0e7e73b029bef10f472a1db302bbc4e3691c3
Brant Strausbaugh <brant.strausbaugh@projectroseland.com>
2025-10-21 06:59:33 -0500

    Initial commit

---
+++ MMM-SchoolMenu.js
@@ -0,0 +1,88 @@
+function updateMenus(payload) {
+  let thisWeekDiv = document.getElementById('thisweek');
+  const header1 = document.createElement('div');
+  header1.id = `week-0`;
+  header1.classList.add('header');
+  header1.innerHTML = `Week of ${payload.thisWeek.weekOf}`;
+  thisWeekDiv.appendChild(header1);
+  payload.thisWeek.menu.forEach((day, index) => {
+    const dayDiv = document.createElement('div');
+    dayDiv.id = `day-0-${index}`;
+    dayDiv.classList.add('day');
+    dayDiv.innerHTML = dayjs(day.date).format('dddd');
+    thisWeekDiv.appendChild(dayDiv);
+    day.items.forEach((item, itemIdx) => {
+      const lunchDiv = document.createElement('div');
+      lunchDiv.id = `lunch-0-${index}-${itemIdx}`;
+      lunchDiv.classList.add('item');
+      lunchDiv.innerHTML = item;
+      thisWeekDiv.appendChild(lunchDiv);
+    });
+  });
+
+  let nextWeekDiv = document.getElementById('nextweek');
+  const header2 = document.createElement('div');
+  header2.id = `week-1`;
+  header2.classList.add('header');
+  header2.innerHTML = `Week of ${payload.nextWeek.weekOf}`;
+  nextWeekDiv.appendChild(header2);
+
+  payload.nextWeek.menu.forEach((day, index) => {
+    const dayDiv = document.createElement('div');
+    dayDiv.id = `day-1-${index}`;
+    dayDiv.classList.add('day');
+    dayDiv.innerHTML = dayjs(day.date).format('dddd');
+    nextWeekDiv.appendChild(dayDiv);
+    const lunchDiv = document.createElement('div');
+    lunchDiv.id = `lunch-1-${index}`;
+    lunchDiv.classList.add('item');
+    lunchDiv.innerHTML = day.items.join(', ');
+    nextWeekDiv.appendChild(lunchDiv);
+  });
+}
+
+Module.register('MMM-SchoolMenu', {
+  start: function () {
+    Log.log('Starting module: ' + this.name);
+
+    this.getMenus(this);
+  },
+
+  getMenus: function (_this) {
+    // Make the initial request to the helper then set up the timer to perform the updates
+    _this.sendSocketNotification('GET_WEEKLY_MENUS', {
+      locationId: _this.config.locationId,
+    });
+
+    const now = dayjs();
+    const nextDay = now.add(1, 'day').startOf('day');
+
+    setTimeout(_this.getMenus, nextDay.diff(now), _this);
+  },
+
+  getScripts: function () {
+    return ['dayjs.js'];
+  },
+
+  getStyles: function () {
+    return ['MMM-SchoolMenu.css'];
+  },
+
+  getDom: function () {
+    const wrapper = document.createElement('div');
+    wrapper.id = 'school-menu-wrapper';
+    wrapper.classList.add('MMM-SchoolMenu');
+    wrapper.innerHTML =
+      '<table><tr><td class="column"><div id="thisweek" class="week"/></td><td class="column"><div id="nextweek" class="week"/></td></tr>';
+
+    return wrapper;
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'GOT_WEEKLY_MENUS') {
+      if (payload) {
+        updateMenus(payload);
+      }
+    }
+  },
+});
---
+++ node_helper.js
@@ -0,0 +1,106 @@
+var NodeHelper = require('node_helper');
+var undici = require('undici');
+var dayjs = require('dayjs');
+
+const getMenuForWeek = async (locationId, startOfWeek) => {
+  return new Promise((resolve, reject) => {
+    const url = `https://psd202.schooldish.com/api/menu/GetMenuPeriods?locationId=${locationId}&storeId=&date=${startOfWeek.format(
+      'MM/DD/YYYY'
+    )}&mode=Weekly`;
+    console.log('URL', url);
+    undici
+      .request(url, {
+        method: 'GET',
+      })
+      .then((periodsResponse) => {
+        if (periodsResponse.statusCode > 299) {
+          throw new Error('unable to get menu periods');
+        }
+        return periodsResponse.body;
+      })
+      .then((periodsBody) => periodsBody.json())
+      .then((periods) => {
+        const lunchPeriod = periods.Result.filter(
+          (period) => period.PeriodName === 'Lunch'
+        );
+        if (lunchPeriod.length > 0) {
+          undici
+            .request(
+              `https://psd202.schooldish.com/api/menu/GetMenus?locationId=${locationId}&storeIds=&mode=Weekly&date=${startOfWeek.format(
+                'MM/DD/YYYY'
+              )}&time=&periodId=${lunchPeriod[0].PeriodId}&fulfillmentMethod=`
+            )
+            .then((menuResponse) => {
+              if (menuResponse.statusCode > 299) {
+                throw new Error('error getting lunch menu');
+              }
+              return menuResponse.body;
+            })
+            .then((menuBody) => menuBody.json())
+            .then((menu) => {
+              const menuResult = [];
+              const entreeStations = menu.Menu?.MenuStations?.filter(
+                (station) =>
+                  (station.Name === 'Entree' || station.Name === 'Grill') &&
+                  station.PeriodId === lunchPeriod[0].PeriodId + ''
+              );
+              const menuMap = new Map();
+              if (entreeStations && entreeStations.length > 0) {
+                entreeStations.forEach((entryStation) => {
+                  menu.Menu?.MenuProducts?.filter(
+                    (menuProduct) =>
+                      menuProduct.StationId === entryStation.StationId
+                  ).forEach((menuItem) => {
+                    if (!menuMap.has(menuItem.AssignedDate)) {
+                      menuMap.set(menuItem.AssignedDate, []);
+                    }
+                    menuMap
+                      .get(menuItem.AssignedDate)
+                      .push(menuItem.Product.MarketingName);
+                  });
+                });
+                menuMap.forEach((value, key) => {
+                  menuResult.push({ date: key, items: value });
+                });
+                menuResult.sort(
+                  (a, b) => dayjs(a.date).valueOf() - dayjs(b.date).valueOf()
+                );
+              }
+              resolve(menuResult);
+            });
+        } else {
+          throw Error('no lunch period found');
+        }
+      })
+      .catch((error) => reject(error));
+  });
+};
+
+module.exports = NodeHelper.create({
+  start: function () {
+    console.log('MMM-SchoolMenu helper, started...');
+  },
+
+  getWeeklyMenu: function (payload) {
+    var _this = this;
+    const thisWeek = dayjs().startOf('week');
+    const monday = thisWeek.day(1);
+    const nextMonday = thisWeek.day(8);
+
+    Promise.all([
+      getMenuForWeek(payload.locationId, monday),
+      getMenuForWeek(payload.locationId, nextMonday),
+    ]).then((menus) => {
+      _this.sendSocketNotification('GOT_WEEKLY_MENUS', {
+        thisWeek: { weekOf: monday.format('MMM DD, YYYY'), menu: menus[0] },
+        nextWeek: { weekOf: nextMonday.format('MMM DD, YYYY'), menu: menus[1] },
+      });
+    });
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'GET_WEEKLY_MENUS') {
+      this.getWeeklyMenu(payload);
+    }
+  },
+});
