8f62a93d020ca3cccfe6534255741a4af0a7a8fa
Brant Strausbaugh <brant.strausbaugh@projectroseland.com>
2026-03-20 11:50:54 -0500

    Remove logging

--- node_helper.js
+++ node_helper.js
@@ -5,9 +5,8 @@ var dayjs = require('dayjs');
 const getMenuForWeek = async (locationId, startOfWeek) => {
   return new Promise((resolve, reject) => {
     const url = `https://psd202.schooldish.com/api/menu/GetMenuPeriods?locationId=${locationId}&storeId=&date=${startOfWeek.format(
-      'MM/DD/YYYY'
+      'MM/DD/YYYY',
     )}&mode=Weekly`;
-    console.log('URL', url);
     undici
       .request(url, {
         method: 'GET',
@@ -21,14 +20,14 @@ const getMenuForWeek = async (locationId, startOfWeek) => {
       .then((periodsBody) => periodsBody.json())
       .then((periods) => {
         const lunchPeriod = periods.Result.filter(
-          (period) => period.PeriodName === 'Lunch'
+          (period) => period.PeriodName === 'Lunch',
         );
         if (lunchPeriod.length > 0) {
           undici
             .request(
               `https://psd202.schooldish.com/api/menu/GetMenus?locationId=${locationId}&storeIds=&mode=Weekly&date=${startOfWeek.format(
-                'MM/DD/YYYY'
-              )}&time=&periodId=${lunchPeriod[0].PeriodId}&fulfillmentMethod=`
+                'MM/DD/YYYY',
+              )}&time=&periodId=${lunchPeriod[0].PeriodId}&fulfillmentMethod=`,
             )
             .then((menuResponse) => {
               if (menuResponse.statusCode > 299) {
@@ -42,14 +41,14 @@ const getMenuForWeek = async (locationId, startOfWeek) => {
               const entreeStations = menu.Menu?.MenuStations?.filter(
                 (station) =>
                   (station.Name === 'Entree' || station.Name === 'Grill') &&
-                  station.PeriodId === lunchPeriod[0].PeriodId + ''
+                  station.PeriodId === lunchPeriod[0].PeriodId + '',
               );
               const menuMap = new Map();
               if (entreeStations && entreeStations.length > 0) {
                 entreeStations.forEach((entryStation) => {
                   menu.Menu?.MenuProducts?.filter(
                     (menuProduct) =>
-                      menuProduct.StationId === entryStation.StationId
+                      menuProduct.StationId === entryStation.StationId,
                   ).forEach((menuItem) => {
                     if (!menuMap.has(menuItem.AssignedDate)) {
                       menuMap.set(menuItem.AssignedDate, []);
@@ -63,7 +62,7 @@ const getMenuForWeek = async (locationId, startOfWeek) => {
                   menuResult.push({ date: key, items: value });
                 });
                 menuResult.sort(
-                  (a, b) => dayjs(a.date).valueOf() - dayjs(b.date).valueOf()
+                  (a, b) => dayjs(a.date).valueOf() - dayjs(b.date).valueOf(),
                 );
               }
               resolve(menuResult);
